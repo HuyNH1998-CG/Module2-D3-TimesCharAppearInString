95612089b0bdb724bade15ee44357534d942fe7e
zhuy-nguyenz <graio911@gmail.com>
2021-06-08 16:00:51 +0700

    Times Appear Of Character

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,21 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String toCheck = input.nextLine();
+        System.out.println("Enter a character to find");
+        char X = input.next().charAt(0);
+        int count = 0;
+        for (int i = 0; i < toCheck.length(); i++) {
+            if (toCheck.charAt(i) == X) {
+                count++;
+            }
+        }
+        System.out.println("There are " + count);
+    }
+}
